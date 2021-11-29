d154ac47e2ec7f48eeb45b39363c2ee387bd7fc7
Eloisa Hernandez <eloisa.hdez.r@gmail.com>
2021-11-29 18:05:35 +0100

    Updated UI and implemented data transform functions

--- classipyapp/app_functions.py
+++ classipyapp/app_functions.py
@@ -1,24 +1,37 @@
 import streamlit as st
 import pandas as pd
 
+
+#Display summary
+def summary(df):
+    col1,col2,col3 = st.columns(3)
+    col1.metric('Columns', len(df.columns))
+    col2.metric('Rows', len(df))
+    col3.metric('','')
+
 #Takes a list of column names, type and transformations available and returns
-#column names with options to transform
+# dictionary with column name as key and a tuple of columns to include and transformation
 def display_transformation_options(column_names, column_type,
                                    transformation_types):
     st.markdown(f'''#### {column_type}''')
     st.write("➖" * 35)
     with st.expander("Expand"):
+        transformation_dict = {}
         st.markdown(f'''##### Select Transformation''')
-
         for name in column_names:
-            st.markdown(f'###### {name}')
+            col1, col2, col3, col4 = st.columns(4)
+            to_include = col1.checkbox('Include', key = name)
+            col2.markdown(f'###### {name}')
+            col3.markdown(f'###### cat-multi')
             st.markdown(
                 """<style>[data-baseweb="select"] {margin-top: -40px;}
                         </style>
                     """,
                 unsafe_allow_html=True,
             )
-            transformation = st.selectbox(" ", transformation_types, key=name)
+            transformation = col4.selectbox(" ", transformation_types, key = name)
+            transformation_dict[name] = (to_include,transformation)
+    return transformation_dict
 
 
 
@@ -33,7 +46,7 @@ def processing_feedback(status):
 
 #Allow user to download output as csv:
 # 1. Convert df to csv
-#@st.cache - add cache before function to prevent computation on every rerun
+@st.cache
 def convert_df(transformed_df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return transformed_df.to_csv().encode('utf-8')
