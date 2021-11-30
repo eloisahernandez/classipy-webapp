143a5994ad34783cfe88510a399374b051d9c7c3
Eloisa Hernandez <eloisa.hdez.r@gmail.com>
2021-11-30 11:36:02 +0100

    Implemented json api call and classipy DataFrameTransformer

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 from classipyapp.app_functions import convert_df, display_transformation_options, download_button, summary
-from classipyapp.data_transform import get_dataframe
+from classipy import DataFrameTransformer
+import requests
 
 st.set_page_config(
-    page_title="Quick reference",  # => Quick reference - Streamlit
+    page_title="Classipy",
     page_icon="🎲",
     layout="centered",  # wide,centered
     initial_sidebar_state="auto")  # collapsed
@@ -17,10 +18,17 @@ st.markdown('''
 uploaded_file = st.file_uploader("Upload your csv file",
                                  type=["csv", "json"],
                                  accept_multiple_files=False)
+
+
 if uploaded_file is not None:
     uploaded_df = pd.read_csv(uploaded_file)
 try:
-    transformed_df = get_dataframe(uploaded_df)
+    dataset_name_tmp = 'no_dataset_name'
+    table_name = uploaded_file.name.split('.csv')[0]
+    transformed_df = DataFrameTransformer(dataset_name_tmp,table_name).fit_transform(uploaded_df)
+    r = requests.post('https://classipy-s6bveudxoq-ew.a.run.app/summary_predict',
+    json = transformed_df.to_json())
+    #st.write(f"Status Code: {r.status_code}, Response: {r.json()}")
 except:
     pass
 
@@ -52,10 +60,10 @@ column_names_n = ['Column3','Column4', 'Column6']
 categorical_transformation =  ['OneHotEncoder','LabelEncoder']
 numerical_transformation = ['StandardScaling','MinMaxValue']
 
-#st.text(display_transformation_options(column_names_c, 'Categorical',
-#                             categorical_transformation))
-#st.text(display_transformation_options(column_names_n, 'Numerical',
-#                               numerical_transformation))
+st.text(display_transformation_options(column_names_c, 'Categorical',
+                             categorical_transformation))
+st.text(display_transformation_options(column_names_n, 'Numerical',
+                               numerical_transformation))
 
 try:
     download_button(transformed_df)
