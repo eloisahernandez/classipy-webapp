0a604b731314b87e1a117e1f107c92f373c0da80
Eloisa Hernandez <eloisa.hdez.r@gmail.com>
2021-12-01 18:26:54 +0100

    Fixed API and implemented parsing functions

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+from streamlit.state.session_state import SessionState
 from classipyapp.app_functions import api_post_call, display_transformation_options, download_button, summary, transform_data
-
+from classipyapp.parsing import Parsing
 
 st.set_page_config(
     page_title="Classipy",
@@ -27,7 +28,9 @@ option_1 = 'Get Summary & Predictions (select transformations)'
 option_2 = 'Get Summary - Clean & Transform (output with recommended transformations)'
 selection = st.radio('Select an action:', (option_1,option_2))
 
+
 submit_button = st.button('Submit')
+
 
 #column_names = []
 #labels = pd.Series(
@@ -37,11 +40,12 @@ submit_button = st.button('Submit')
 if (submit_button and (selection == option_1) and uploaded_file is not None) or 'postsubmit' in st.session_state:
     st.session_state['postsubmit'] = True
     st.markdown('''### Summary''')
-    transformed_df, column_names = transform_data(uploaded_df, file_name)
-    labels = api_post_call(transformed_df)
     summary(uploaded_df)
-    transf_dict = display_transformation_options(column_names, labels)
-    st.write(transf_dict)
+    transformed_df, column_names = transform_data(uploaded_df, file_name)
+    if 'labels' not in st.session_state:
+        st.session_state['labels'] = api_post_call(transformed_df)
+    labels = st.session_state['labels']
+    transf_dict = display_transformation_options(column_names,labels)
 elif (submit_button and uploaded_file is None):
     st.warning('Please upload a file')
 elif submit_button and (selection == option_2):
@@ -52,6 +56,14 @@ else:
     st.warning('Please select an action and click Submit')
 
 
+# Implement parser and transformer after user confirms selections
+
+transform_button = st.button('Transform')
+
+if transform_button:
+    parse_type = Parsing(transf_dict)
+    transformed_df, status = parse_type.parse_and_transform(uploaded_df)
+    st.write(status)
 
 try:
     download_button(transformed_df)
